2edd46804d364e9dbeacc3d43da30a7662a371a5
Han Ho <han.ho@plenti.com.au>
2022-07-19 17:53:35 +0700

    Init project

---
+++ src/views/team-cart/index.js
@@ -0,0 +1,17 @@
+import { Grid, Item } from '@mui/material';
+import MainCard from 'ui-component/cards/MainCard';
+
+const TeamCart = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={8}>
+                ABC
+            </Grid>
+            <Grid item xs={4}>
+                Bill Here
+            </Grid>
+        </Grid>
+    );
+};
+
+export default TeamCart;
